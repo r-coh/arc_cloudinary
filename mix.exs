484e21662ebe6ba5824a8f60badfb2b94a3b1a96
Erickson Gitahi <moshericoe@gmail.com>
2020-10-08 16:10:56 +0300

    Setup package info

--- mix.exs
+++ mix.exs
@@ -4,26 +4,40 @@ defmodule ArcCloudinary.MixProject do
   def project do
     [
       app: :arc_cloudinary,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      description: description(),
+      package: package()
+    ]
+  end
+
+  defp description do
+    """
+    Provides Cloudinary storage backend for Arc.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Erickson Gitahi"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/_r.coh/arc_cloudinary"},
+      files: ~w(mix.exs README.md lib config)
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger],
-      mod: {ArcCloudinary.Application, []}
-    ]
+    [applications: [:logger]]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:jason, "~> 1.2"},
       {:cloudex, "~> 1.4"},
       {:arc, "~> 0.11.0"},
